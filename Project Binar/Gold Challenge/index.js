4ae604032afbdf173052dd78ee944bae534925a5
Faisal Baqir Tahmidi <faishalb0@gmail.com>
2023-08-28 22:16:31 +0700

    add http method get

--- Project Binar/Gold Challenge/index.js
+++ Project Binar/Gold Challenge/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+const db = require('./db/db');
 
+app.get('/api/products', async (req, res) => {
+    try {
+        // Select operation
+        const data = await db.select('*').from('products');
+
+        res.json({
+            status: 200,
+            data: data
+        })
+    } catch (error) {
+        res.json({
+            status : 500,
+            message: error.message
+        });
+    }
+})
 
 const PORT = 3000;
 app.listen(PORT, (req, res)=> {
