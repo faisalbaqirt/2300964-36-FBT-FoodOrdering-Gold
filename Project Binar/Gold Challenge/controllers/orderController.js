573db69edf2aac8c20b30c4dc6dd56e60557b88a
Faisal Baqir Tahmidi <faishalb0@gmail.com>
2023-08-30 22:11:52 +0700

    update controllers

--- Project Binar/Gold Challenge/controllers/orderController.js
+++ Project Binar/Gold Challenge/controllers/orderController.js
@@ -2,33 +2,57 @@ const db = require('../db/db');
 
 class Controller {
     async createOrder(req, res) {
-        const { customer_id, basket_id, date, shipper_id, fulfilled, total_price, payment_id } = req.body;
-        const newOrder = await db.query('INSERT INTO orders (customer_id, basket_id, date, shipper_id, fulfilled, total_price, payment_id) values ($1, $2, $3, $4, $5, $6, $7) RETURNING *', [customer_id, basket_id, date, shipper_id, fulfilled, total_price, payment_id])
-        res.json(newOrder.rows[0]);
+        try{
+            const {product_name, quantity, name, telephone, address} = req.body
+            
+            const product = await db('products').where('name', req.body.product_name).first();
+    
+            if (!product) {
+                return res.status(404).json({ message: 'Produk tidak ditemukan' });
+            }
+    
+            const total_amount = product.price * quantity;
+            
+            await db('orders').insert({
+                product_id: product.id,
+                product_name: product.name,
+                quantity: quantity,
+                total_amount: total_amount,
+                name: name,
+                telephone: telephone,
+                address: address
+            })
+    
+            res.status(201).redirect('/');
+        } catch (error) {
+            res.json({
+                status : 500,
+                message: error.message
+            });
+        }
     }
 
-    async getOrderByCustomer(req, res) {
-        const id = req.query.id;
-        const order = await db.query('SELECT * FROM orders WHERE customer_id = $1', [id]);
-        res.json(order.rows[0]);
-    } //http://localhost:8080/order?id=1
+    async order(req, res) {
+        res.render('order.ejs')
+    }
+    
+    // async getOrderByCustomer(req, res) {
+    //     const id = req.query.id;
+    //     const order = await db.query('SELECT * FROM orders WHERE customer_id = $1', [id]);
+    //     res.json(order.rows[0]);
+    // } 
 
-    // async getOneOrder(req, res) {
+    // async updateOrder(req, res) {
     //     const id = req.params.id;
-    //     const order = await db.query('SELECT * FROM orders WHERE id = $1', [id]);
+    //     const { customer_id, basket_id, date, shipper_id, fulfilled, total_price, payment_id } = req.body;
+    //     const order = await db.query('UPDATE orders SET customer_id = $1, basket_id = $2, date = $3, shipper_id = $4, fulfilled = $5, total_price = $6, payment_id = $7 WHERE id = $8 RETURNING *', [customer_id, basket_id, date, shipper_id, fulfilled, total_price, payment_id, id]);
     //     res.json(order.rows[0]);
     // }
-    async updateOrder(req, res) {
-        const id = req.params.id;
-        const { customer_id, basket_id, date, shipper_id, fulfilled, total_price, payment_id } = req.body;
-        const order = await db.query('UPDATE orders SET customer_id = $1, basket_id = $2, date = $3, shipper_id = $4, fulfilled = $5, total_price = $6, payment_id = $7 WHERE id = $8 RETURNING *', [customer_id, basket_id, date, shipper_id, fulfilled, total_price, payment_id, id]);
-        res.json(order.rows[0]);
-    }
-    async deleteOrder(req, res) {
-        const id = req.params.id;
-        const order = await db.query('DELETE FROM orders WHERE id = $1', [id]);
-        res.json(order.rows[0]);
-    }
+    // async deleteOrder(req, res) {
+    //     const id = req.params.id;
+    //     const order = await db.query('DELETE FROM orders WHERE id = $1', [id]);
+    //     res.json(order.rows[0]);
+    // }
 
 };
 
