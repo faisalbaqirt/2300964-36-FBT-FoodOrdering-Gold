39107377c637f79e81cf4790c466f7c04ceb0efb
Faisal Baqir Tahmidi <faishalb0@gmail.com>
2023-08-30 22:13:42 +0700

    update server

--- Project Binar/Gold Challenge/index.js
+++ Project Binar/Gold Challenge/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const db = require('./db/db');
+const orderRouter = require('./routes/orderRoutes')
+app.use(express.json());
 
-app.use(express.json())
+app.set('view-engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.use(orderRouter)
 
 // membaca semua data produk yang ada di database
 app.get('/api/products', async (req, res) => {
