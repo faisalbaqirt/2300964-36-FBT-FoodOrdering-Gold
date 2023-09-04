5c672426f8086921711d57e6d79ebb41bf7731a3
Faisal Baqir Tahmidi <faishalb0@gmail.com>
2023-09-05 00:38:16 +0700

    update

--- Project Binar/Gold Challenge/controllers/orderController.js
+++ Project Binar/Gold Challenge/controllers/orderController.js
@@ -36,23 +36,6 @@ class Controller {
         res.render('order.ejs')
     }
     
-    // async getOrderByCustomer(req, res) {
-    //     const id = req.query.id;
-    //     const order = await db.query('SELECT * FROM orders WHERE customer_id = $1', [id]);
-    //     res.json(order.rows[0]);
-    // } 
-
-    // async updateOrder(req, res) {
-    //     const id = req.params.id;
-    //     const { customer_id, basket_id, date, shipper_id, fulfilled, total_price, payment_id } = req.body;
-    //     const order = await db.query('UPDATE orders SET customer_id = $1, basket_id = $2, date = $3, shipper_id = $4, fulfilled = $5, total_price = $6, payment_id = $7 WHERE id = $8 RETURNING *', [customer_id, basket_id, date, shipper_id, fulfilled, total_price, payment_id, id]);
-    //     res.json(order.rows[0]);
-    // }
-    // async deleteOrder(req, res) {
-    //     const id = req.params.id;
-    //     const order = await db.query('DELETE FROM orders WHERE id = $1', [id]);
-    //     res.json(order.rows[0]);
-    // }
 
 };
 
