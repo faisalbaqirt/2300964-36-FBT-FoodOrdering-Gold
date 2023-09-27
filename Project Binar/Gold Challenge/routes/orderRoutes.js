c5da144afd99ec7513fd6b4510afe88ef3bb12b3
Faisal Baqir Tahmidi <137419449+faisalbaqirt@users.noreply.github.com>
2023-09-27 20:19:25 +0700

    Delete Project Binar/Gold Challenge directory

--- Project Binar/Gold Challenge/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Controller = require('../controllers/orderController');
-
-router.post('/order', Controller.createOrder);
-router.get('/order', Controller.order);
-
-module.exports = router;
